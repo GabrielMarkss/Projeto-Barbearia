9d1ae93d1ed871f51795c685e76b3680da71ecfe
Gabriel Marques <129567827+GabrielMarkss@users.noreply.github.com>
2025-02-19 13:48:38 -0300

    Add files via upload

---
+++ StudioMoneyBarber/src/app/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class DashboardComponent {}
